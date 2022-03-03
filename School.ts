ac77f66fa03e410812e2446f11c739d334953cf1
miamoo21 <maleonora.mendenilla@gmail.com>
2022-03-03 21:42:56 +0800

    Done Activity

--- School.ts
+++ School.ts
@@ -1,4 +1,4 @@
-import { Student } from "./Student";
+import { Student } from "./Student"
 import { StudentList } from "./StudentList";
 
 const student1 = new Student(
@@ -44,3 +44,8 @@ console.log(studentList.getStudents());
 console.log(studentList.updateAge("2015-105869", 25));
 // console.log(studentList.checkStudentId("2015-105870"));
 console.log(studentList.getStudents());
+console.log(studentList.updateCollege("2015-105869", "URSB"));
+console.log(studentList.updateAddress("2015-105869", "Taytay Rizal"));
+console.log(studentList.updateCourse("2015-105867", "BSIT"));
+console.log(studentList.updateGender("2015-105869", "FEMALE"));
+console.log(studentList.removeStudent("2015-105869"));
